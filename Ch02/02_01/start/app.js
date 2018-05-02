ce5e4301e4b449fe714aa27135f0659791bf12b7
Krunal Patel <kpate236@uic.edu>
2018-05-02 19:57:46 +0000

    2.3

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -1,6 +1,27 @@
 (function() {
   "use strict";
 
-  // Start here
+  function ProductImage(props) {
+    return React.createElement("img", {
+      src: "../../../assets/red.jpg",
+      alt: "Product Image"
+    });
+  }
 
+  function ProductCustomizer(props) {
+    return React.createElement(
+      "div",
+      { className: "customizer" },
+      React.createElement(
+        "div",
+        { className: "product-image" },
+        React.createElement(ProductImage)
+      )
+    );
+  }
+
+  ReactDOM.render(
+    React.createElement(ProductCustomizer),
+    document.getElementById("react-root")
+  );
 })();
