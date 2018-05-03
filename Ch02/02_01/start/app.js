3d87e27efe945da82531059f15e411dc5416d544
Krunal Patel <kpate236@uic.edu>
2018-05-03 00:33:19 +0000

    2.7

--- Ch02/02_01/start/app.js
+++ Ch02/02_01/start/app.js
@@ -3,12 +3,10 @@
 
   function SizeSelector(props) {
     function sizeOptions() {
-      var sizes = window.Inventory.allSizes;
-
-      return sizes.map(function(num) {
+      return props.sizes.map(function(num) {
         return (
           <option value={num} key={num}>
-            Size: {num}
+            {num}
           </option>
         );
       });
@@ -25,21 +23,35 @@
   }
 
   function ProductImage(props) {
-    return <img src={"../../../assets/" + props.color + ".jpg"} alt="Product Image" />;
-  }
-
-  function ProductCustomizer(props) {
     return (
-      <div className="customizer">
-        <div className="product-image">
-          <ProductImage color="red"/>
-        </div>
-        <div className="selectors">
-          <SizeSelector size={9}/>
-        </div>
-      </div>
+      <img src={`../../../assets/${props.color}.jpg`} alt="Product Image" />
     );
   }
+
+  var ProductCustomizer = createReactClass({
+    getInitialState: function() {
+      var sizes = window.Inventory.allSizes;
+
+      return {
+        color: "red",
+        size: 8,
+        sizes: sizes
+      };
+    },
+
+    render: function() {
+      return (
+        <div className="customizer">
+          <div className="product-image">
+            <ProductImage color={this.state.color} />
+          </div>
+          <div className="selectors">
+            <SizeSelector size={this.state.size} sizes={this.state.sizes} />
+          </div>
+        </div>
+      );
+    }
+  });
 
   ReactDOM.render(<ProductCustomizer />, document.getElementById("react-root"));
 })();
